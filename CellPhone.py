3588edd3d4d99f8734b7bed81b14f76d552e9e4e
cegarc5579 <carlos_garcia1@baylor.edu>
2022-02-14 14:26:14 -0600

    corrected cellphone

--- CellPhone.py
+++ CellPhone.py
@@ -7,11 +7,11 @@ def main():
 
     manname = input("What is the manufacturer name?")
     phmodel = input("Enter the model number:")
-    rprice = input("Enter the price of the phone:")
+    rprice = float(input("Enter the price of the phone:"))
 
     phone = pc.cellphone(manname,phmodel,rprice)
-    print("Manufacturer:" + phone.get__manufact())
-    print("Model Number:" + phone.get__model())
-    print("Retail Price: $" + '%.2f'% phone.get__retail_price())
+    print("Manufacturer:" + phone.get_manufact())
+    print("Model Number:" + phone.get_model())
+    print("Retail Price: $" + '%.2f'% phone.get_retail_price())
 
 main()
