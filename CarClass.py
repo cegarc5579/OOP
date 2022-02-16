acc1cbf371591962ee6b78c7f34273b8fe948b6d
cegarc5579 <carlos_garcia1@baylor.edu>
2022-02-16 16:02:06 -0600

    made changes to carprogram

--- CarClass.py
+++ CarClass.py
@@ -4,6 +4,12 @@ class Car():
         self.__year_model = year
         self.__make = carmake 
         self.__speed = 0
+
+    def get_yearmodel(self):
+        return self.__year_model
+    
+    def get_make(self):
+        return self.__make
     
     def accelerate(self):
         self.__speed += 5
--- CarProgram.py
+++ CarProgram.py
@@ -2,8 +2,9 @@
 import CarClass as cc 
 
 def main():
-    vehicle = cc.Car(2019, "Ford Fusion")
-
+    vehicle = cc.Car("2019", "Ford Fusion")
+    
+    print("Vehicle Year:", vehicle.get_yearmodel() + ' ' + "Vehicle Make:", vehicle.get_make())
     for c in range(1,6):
         vehicle.accelerate()
         print("The car is moving:", vehicle.get_speed())
