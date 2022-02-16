82b128a8f9fe191042af18fac842f3a2ba37c26a
cegarc5579 <carlos_garcia1@baylor.edu>
2022-02-15 21:08:17 -0600

    finished student

--- StudentClass.py
+++ StudentClass.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 class Student: 
     
     def __init__(self, studentid, name, dateob, classif):
@@ -5,4 +7,21 @@ class Student:
         self.__name = name 
         self.__dateofb = dateob
         self.__classification = classif
+        self.__age = 0
+
+    def age(self):
+        today = date.today()
+        self.__age = today.year-int(self.__dateofb)
+    
+    def get_age(self):
+        return self.__age
+    
+    def registrate(self):
+        if self.__classification == 'F':
+            print("Registration Open: 11/10-11/12")
+        elif self.__classification == 'Jr':
+            print("Registration Open: 11/4-11/6")
+        else:
+            print("Registration Open: 11/1-11/3")
+    
     
--- StudentProgram.py
+++ StudentProgram.py
@@ -1,3 +1,17 @@
+from datetime import datetime
 import StudentClass as st
 
-mystu = st.Student()
+#mystu = st.Student()
+def main():
+    studentid = input("What is your student ID?")
+    dateob = int(input("What year were you born?"))
+    name = input("What is your name?")
+    classif = input("What is your classification?")
+
+    st1 = st.Student(studentid,name,dateob,classif)
+    st1.age()
+    print("The students age is:", st1.get_age())
+
+    st1.registrate()
+
+main()
